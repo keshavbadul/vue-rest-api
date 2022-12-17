682ed241bf4f72b151548e05637575d15d2abbb9
keshavbadul <keshav.badul06@gmail.com>
2022-12-17 13:16:25 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,25 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+    <div class="max-w-7xl mx-auto">
+      <nav class="py-4 px-2">
+        <RouterLink  
+          class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white" 
+          to="/"
+          >
+          Home
+        </RouterLink>
+        <RouterLink 
+          class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white"
+          to="/skills"
+        >
+        Skills
+        </RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main class="max-w-7xl mx-auto min-h-screen">
+    <RouterView />
+  </main>
 </template>
---
+++ src/views/skills/SkillEdit.vue
@@ -0,0 +1,44 @@
+<script setup>
+    import { onMounted } from 'vue'; 
+    import useSkills from '../../composables/skills';
+
+    const { skill, getSkill, updateSkill, errors } = useSkills();
+
+    const props = defineProps({
+        id: {
+            required: true,
+            type: String,
+        }
+    });
+
+    onMounted(() => getSkill(props.id));
+</script>
+<template>
+    <div class="mt-12">
+        <form @submit.prevent="updateSkill($route.params.id)" class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md">
+            <div class="space-y-6">
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                    <input v-model="skill.name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div class="" v-if="errors.name">
+                        <div class="text-sm text-red-400">{{ errors.name[0] }}</div>
+                    </div>
+                </div>
+
+                <div class="mb-6">
+                    <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
+                    <input v-model="skill.slug" type="text" id="slug" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div class="" v-if="errors.slug">
+                        <div class="text-sm text-red-400">{{ errors.slug[0] }}</div>
+                    </div>
+                </div>
+
+                <div class="mt-4">
+                    <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">
+                        Store
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
